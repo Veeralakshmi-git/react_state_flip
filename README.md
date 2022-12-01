74dd83b5a30b1f21389ef4490a2e4a027545f0dc
Veeralakshmi-r1 <115619349+Veeralakshmi-git@users.noreply.github.com>
2022-12-01 14:24:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react_state_flip
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-5teziy)
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return(
+  
+    <div class="container">
+      <p className="text-title">Do you want to flipp the button?</p>
+      <div className="toggle-button">Yes</div>
+    </div>
+
+  ) 
+  
+}
+
+export default App
